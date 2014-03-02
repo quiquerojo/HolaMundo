85da4a06ee13c21bd600cff06aaaabf67a1f8577
quiquerojo <quiquerojoies@gmail.com>
2014-03-02 20:42:39 +0100

    Inicial

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author admin
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * 
+	 */
+	public HolaMundo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+
+	}
+
+}
