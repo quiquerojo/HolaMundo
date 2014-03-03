e3a3b4407efd3dce428863b22710f0f6123d3128
quiquerojo <quiquerojoies@gmail.com>
2014-03-03 23:31:57 +0100

    Hemos añadido un saludo personalizado al programa preguntando el nombre

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -9,11 +11,18 @@
 public class HolaMundo {
 
 	
+	private static Scanner entrada;
+
 	/**Programa que saluda al mundo
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		entrada = new Scanner(System.in);
+		String nombre = null;
 		System.out.println("Hola Mundo");
+		System.out.print("Dame tu nombre: ");
+		nombre = entrada.nextLine();
+		System.out.println("De todo el mundo, te saludo a tí, "+nombre);
 
 	}
 
